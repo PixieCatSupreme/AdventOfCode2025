2a5652a24448928f404c6df57a799e9c3f24db5a
PixieCatSupreme <cynthia.steenvoorden@gmail.com>
2025-12-01 16:11:40 +0100

    Finished day 1-1 and 1-2

---
+++ Day1-1/Program.cs
@@ -0,0 +1,50 @@
+﻿int dial = 50;
+int zeroCount = 0;
+
+Console.WriteLine("Reading dail input!");
+
+string[] input = File.ReadAllLines("./input.txt");
+
+Console.WriteLine($"Starting dial at {dial}.");
+
+foreach (string c in input)
+{
+    bool isRight = c.First() == 'R';
+    int newVal = -1;
+
+    Console.WriteLine($"Doing input {c}!");
+
+    if (!int.TryParse(c.AsSpan(1), out int value))
+    {
+        Console.WriteLine($"Unable to parse {c} as valid input!");
+        continue;
+    }
+
+    if (isRight)
+    {
+        newVal = (dial + value) % 100;
+
+        Console.WriteLine($"Turned dial from {dial} up to {newVal}.");
+        dial = newVal;
+    }
+    else
+    {
+        newVal = (dial - value) % 100;
+
+        if(newVal < 0)
+        {
+            newVal += 100;
+        }
+
+
+        Console.WriteLine($"Turned dial from {dial} down to {newVal}.");
+        dial = newVal;
+    }
+
+    if (dial == 0)
+    {
+        zeroCount++;
+    }
+}
+
+Console.WriteLine($"Amount of zeroes: {zeroCount}.");
---
+++ Day1-2/Program.cs
@@ -0,0 +1,80 @@
+﻿int dial = 50;
+int zeroCount = 0;
+
+Console.WriteLine("Reading dail input!");
+
+string[] input = File.ReadAllLines("./input.txt");
+
+Console.WriteLine($"Starting dial at {dial}.");
+
+foreach (string c in input)
+{
+    bool isRight = c.First() == 'R';
+    int newVal = -1;
+
+    if (!int.TryParse(c.AsSpan(1), out int value))
+    {
+        Console.WriteLine($"Unable to parse {c} as valid input!");
+        continue;
+    }
+
+    while (value > 100)
+    {
+        zeroCount++;
+        value -= 100;
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"Dial rolled over zero.");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    if (isRight)
+    {
+        if(dial + value > 100)
+        {
+            zeroCount++;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Dial rolled over zero.");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        newVal = (dial + value) % 100;
+
+        Console.WriteLine($"Turned dial from {dial} up to {newVal} ({c}).");
+        dial = newVal;
+    }
+    else
+    {
+        bool isZero = dial == 0;
+        newVal = (dial - value) % 100;
+
+        if(newVal < 0)
+        {
+            if(!isZero)
+            {
+                zeroCount++;
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Dial rolled over zero.");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            newVal += 100;
+        }
+
+        Console.WriteLine($"Turned dial from {dial} down to {newVal} ({c}).");
+
+        dial = newVal;
+    }
+
+    if (dial == 0)
+    {
+        zeroCount++;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"Dial is 0.");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
+
+Console.WriteLine($"Amount of zeroes: {zeroCount}.");
