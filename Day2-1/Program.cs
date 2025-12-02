0fde8f9f5e63d19d0981151a2fd8cea6a9e5cadf
PixieCatSupreme <cynthia.steenvoorden@gmail.com>
2025-12-02 14:08:50 +0100

    Completed day 2

---
+++ Day2-1/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("Reading IDs!");
+
+string[] input = File.ReadAllText("./input.txt").Trim().Split(',');
+List<long> invalidIds = [];
+
+foreach (string range in input)
+{
+    string[] values = range.Split('-');
+
+    if (!long.TryParse(values[0], out long first) || !long.TryParse(values[1], out long second))
+    {
+        Console.WriteLine($"Invalid input: '{range}'!");
+        continue;
+    }
+    else
+    {
+        Console.WriteLine($"Checking range '{range}'!");
+    }
+
+    for (long i = first; i <= second; i++)
+    {
+        string s = i.ToString();
+        if (s.Length % 2 != 0)
+        {
+            //Console.WriteLine($"Value '{s}' is valid!");
+            continue;
+        }
+
+        string f = s[..(s.Length / 2)];
+        string l = s[(s.Length / 2)..];
+
+        if (f == l)
+        {
+            Console.WriteLine($"Invalid ID: '{s}'. Sequence is repeated!");
+            invalidIds.Add(i);
+        }
+    }
+}
+
+Console.WriteLine($"Sum of invalid IDs is {invalidIds.Sum()}.");
---
+++ Day2-2/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.WriteLine("Reading IDs!");
+
+string[] input = File.ReadAllText("./input.txt").Trim().Split(',');
+List<long> invalidIds = [];
+
+foreach (string range in input)
+{
+    string[] values = range.Split('-');
+
+    if (!long.TryParse(values[0], out long first) || !long.TryParse(values[1], out long second))
+    {
+        Console.WriteLine($"Invalid input: '{range}'!");
+        continue;
+    }
+    else
+    {
+        Console.WriteLine($"Checking range '{range}'!");
+    }
+
+    for (long i = first; i <= second; i++)
+    {
+        string s = i.ToString();
+        int l = s.Length;
+
+        for (int j = 1; j <= l / 2; j++)
+        {
+            if (l % j != 0)
+            {
+                continue;
+            }
+
+            string check = s[..j];
+            bool invalid = true;
+
+            for (int k = 1; k < l / j; k++)
+            {
+                if (s.Substring(k * j, j) != check)
+                {
+                    invalid = false;
+                    break;
+                }
+            }
+
+            if (invalid) 
+            {
+                invalidIds.Add(i);
+                break;
+            }
+        }
+    }
+
+}
+
+Console.WriteLine($"Sum of invalid IDs is {invalidIds.Sum()}.");
