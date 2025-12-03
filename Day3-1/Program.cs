5fce37cc86fce7979dd4c7d547737e8b31424898
PixieCatSupreme <cynthia.steenvoorden@gmail.com>
2025-12-03 11:46:12 +0100

    Did day 3

---
+++ Day3-1/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Reading battery banks!");
+
+string[] input = File.ReadAllLines("./input.txt");
+List<long> largestJoltages = [];
+
+foreach (var line in input)
+{
+    int[] batteries = [.. line.Select(x => int.Parse(x.ToString()))];
+    int firstBiggest = -1;
+    int secondBiggest = -1;
+
+    int firstBiggestIndex = -1;
+
+    for (int i = 0; i < batteries.Length - 1; i++)
+    {
+        int battery = batteries[i];
+        if (battery > firstBiggest)
+        {
+            firstBiggest = battery;
+            firstBiggestIndex = i;
+        }
+    }
+
+    for (int i = firstBiggestIndex + 1; i < batteries.Length; i++)
+    {
+        int battery = batteries[i];
+        if (battery > secondBiggest)
+        {
+            secondBiggest = battery;
+        }
+    }
+
+    long output = long.Parse($"{firstBiggest}{secondBiggest}");
+    largestJoltages.Add(output);
+
+    Console.WriteLine($"Biggest joltage is {output}.");
+}
+
+Console.WriteLine($"Total output joltage is {largestJoltages.Sum()}.");
---
+++ Day3-2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+Console.WriteLine("Reading battery banks!");
+
+string[] input = File.ReadAllLines("./input.txt");
+List<long> largestJoltages = [];
+int outputLength = 12;
+
+foreach (var line in input)
+{
+    int[] batteries = [.. line.Select(x => int.Parse(x.ToString()))];
+    StringBuilder sb = new();
+
+    int lastIndex = -1;
+
+    while (sb.Length < outputLength)
+    {
+        int biggest = -1;
+
+        for (int i = lastIndex + 1; i < batteries.Length - (outputLength - 1 - sb.Length); i++)
+        {
+            int battery = batteries[i];
+            if (battery > biggest)
+            {
+                biggest = battery;
+                lastIndex = i;
+            }
+        }
+
+        sb.Append(biggest);
+    }
+
+    long output = long.Parse(sb.ToString());
+    largestJoltages.Add(output);
+
+    Console.WriteLine($"Biggest joltage is {output}.");
+}
+
+Console.WriteLine($"Total output joltage is {largestJoltages.Sum()}.");
